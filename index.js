619318f8905c1a92a02b60cac3d5b3dd711d9fb7
Cass Enos <enosdoc@gmail.com>
2026-01-15 09:22:35 -0800

    added multiple choice

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const message = `Welcome to our page!`;
-alert(message);
-console.log(`testing`);
-const isTrue = `testing`;
-console.log(isTrue);
-console.log(Boolean(isTrue));
-console.log(String(isTrue));
-console.log(Number(`4`) + Number(`5`));
-// EVERYTHING IS A BOOLEAN
+// const message = `Welcome to our page!`;
+// alert(message);
+// console.log(`yo`);
+// const isTrue = `hi`;
+// console.log(isTrue);
+// console.log(Boolean(isTrue));
+// console.log(String(isTrue));
+// console.log(Number(`4`) + Number(`5`));
+// // EVERYTHING IS A BOOLEAN
+// const userInput = prompt(`Enter your name`);
+// console.log(userInput);
+// Everything a user puts in a prompt is a String
+const userInput = prompt(`which bear is best?
+    (1) black bear
+    (2) red bear
+    (3) grizzly
+    (4) bunny bear`);
