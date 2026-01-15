91a3ff49c14de370865e197778f565562c73b005
Cass Enos <enosdoc@gmail.com>
2026-01-15 08:13:16 -0800

    updated js file

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const message = `Welcome to our page!`;
 alert(message);
-console.log(Message) message;
+console.log(`testing`);
+const isTrue = `testing`;
+console.log(isTrue);
+console.log(Boolean(isTrue));
+console.log(String(isTrue));
+console.log(Number(`4`) + Number(`5`));
+// EVERYTHING IS A BOOLEAN
